a957bbb1e7257aeb67b630e621c0083dd184e0d9
sugosaboys <lolaloliandecalolaloli@gmail.com>
2025-10-15 22:36:33 +0800

    fixed SocialMedia & Address

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -33,12 +33,12 @@ export interface FooterDefaultTheme extends Struct.ComponentSchema {
     displayName: 'defaultTheme';
   };
   attributes: {
-    Address: Schema.Attribute.Component<'navbar.repeat-text', true>;
+    Address: Schema.Attribute.Component<'navbar.repeat-text', false>;
     BackgroundImage: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios'
     >;
     Phone: Schema.Attribute.String;
-    SocialMedia: Schema.Attribute.Component<'navbar.repeat-text', true>;
+    SocialMedia: Schema.Attribute.Component<'navbar.social-media', true>;
   };
 }
 
@@ -88,6 +88,18 @@ export interface NavbarRepeatText extends Struct.ComponentSchema {
   };
 }
 
+export interface NavbarSocialMedia extends Struct.ComponentSchema {
+  collectionName: 'components_navbar_social_medias';
+  info: {
+    displayName: 'SocialMedia';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    label: Schema.Attribute.String;
+    link: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
@@ -98,6 +110,7 @@ declare module '@strapi/strapi' {
       'menu.menu-theme1': MenuMenuTheme1;
       'navbar.navbar': NavbarNavbar;
       'navbar.repeat-text': NavbarRepeatText;
+      'navbar.social-media': NavbarSocialMedia;
     }
   }
 }
